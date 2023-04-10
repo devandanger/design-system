ed54760d792c4090768fd36675612af4ebad01c2
Evan Anger <devandanger@gmail.com>
2023-04-09 21:08:22 -0400

    Initial revision

---
+++ Example/design_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  design_systemApp.swift
+//  design-system
+//
+//  Created by Evan Anger on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct design_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
